cf256fecd3044bacfb01156c67d1756de7946443
Pujakumari1511 <puja.kumari6494@gmail.com>
2025-01-11 22:29:12 +0200

    fix file name

--- frontend/src/components/employeeCard/EmployeeCard.jsx
+++ frontend/src/components/employeeCard/EmployeeCard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Button from "../buttons/Button"
 import useEmployeeStatus from "../../hooks/useEmployeeStatus";
 import { useNavigate } from "react-router-dom";
